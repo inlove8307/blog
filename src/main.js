e6a34da6b3583d255c7ca19f470895059b7475b4
Kim Taehoon <inlove8307@gmail.com>
2019-12-11 01:16:15 +0000

    add webfont

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import '~/assets/styles/index.scss'
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Noto+Sans+KR:100,300,400,500,700,900&display=swap'
+  })
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
